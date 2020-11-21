4b48474420a39a821b0d9e636a3c79756389f309
Vinicius Cortes <vinihcortes@hotmail.com>
2020-11-21 14:42:23 -0300

    delete .js files

--- src/entities/Gender.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["Female"] = "f";
-    Gender["Male"] = "m";
-})(Gender = exports.Gender || (exports.Gender = {}));
-exports["default"] = Gender;
