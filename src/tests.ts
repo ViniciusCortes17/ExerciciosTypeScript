27e6c024a84d7b3c1b2b80a9220af0dc769faa20
Vinicius Cortes <vinihcortes@hotmail.com>
2020-11-22 13:58:17 -0300

    adding real titles and subtitles

--- src/tests.ts
+++ src/tests.ts
@@ -9,15 +9,15 @@ import Periodical from "./entities/Periodical.js"
 import Gender from "./entities/Gender.js"
 
 
-const person1 = new Person('João' , new Date(1989,12,12) , Gender.Male)
-const person2 = new Person('José', new Date(1985,3,14), Gender.Male)
-const person3 = new Person('Maria', new Date(1994,5,6), Gender.Female)
-const person4 = new Person('Marta', new Date(1979,8,21), Gender.Female)
+const person1 = new Person('Jhon Thomas Teller' , new Date(1940,5,5) , Gender.Male)
+const person2 = new Person('J. R. R. Tolkien', new Date(18,1,3), Gender.Male)
+const person3 = new Person('J. K. Rowling', new Date(1965,7,31), Gender.Female)
+const person4 = new Person('Arthur Conan Doyle', new Date(1859,5,22), Gender.Male)
 
-const Document1 = new Document('titulo qualquer', 'subtitulo qualquer', new Date(2005,5,7), person1)
-const Document2 = new Document('titulo qualquer', 'subtitulo qualquer', new Date(2001,6,8), person2)
-const Document3 = new Document('titulo qualquer', 'subtitulo qualquer', new Date(20017,7,17), person3)
-const Document4 = new Document('titulo qualquer', 'subtitulo qualquer', new Date(1997,1,28), person4)
+const Document1 = new Document('The Life and Death of SAM CROW', 'How the Sons of Anarchy Lost Their Way', new Date(2005,5,7), person1)
+const Document2 = new Document('The Lord of the Rings', 'The Fellowship of the Ring', new Date(1954,7,29), person2)
+const Document3 = new Document('Harry Potter', 'and the Sorcerer`s Stone', new Date(1997,6,26), person3)
+const Document4 = new Document('Sherlock Holmes', 'A Study in Scarlet', new Date(1888,7,23), person4)
 
 const periodical1 = new Periodical(12,1,4,Document1.title,Document1.subtitle,Document1.publishedAt,person1)
 const periodical2 = new Periodical(12,1,4,Document2.title,Document2.subtitle,Document2.publishedAt,person2)
